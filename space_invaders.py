d8c1c89ba94fd73e26e8f59e43e971b93ae1ced8
nigelflippo <flippo.nigel@gmail.com>
2017-12-05 21:08:41 -0700

    Added description

--- space_invaders.py
+++ space_invaders.py
@@ -1,3 +1,4 @@
+"""A space invaders game"""
 import pygame
 from pygame.sprite import Group
 
@@ -23,7 +24,7 @@ def run_game():
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_bullets(bullets)
+		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
 		gf.update_aliens(ai_settings, aliens)
 
 		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
